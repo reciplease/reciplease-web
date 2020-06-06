bdae06d69868f810ed4e7579f63c32228e6aa413
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-06-06 21:10:45 +0100

    Add test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">{ping}</h1>
+        <h1 data-testid="app-title" className="App-title">{ping}</h1>
       </header>
     </div>
   );
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByTestId } = render(<App />);
+  const linkElement = getByTestId("app-title");
   expect(linkElement).toBeInTheDocument();
 });
