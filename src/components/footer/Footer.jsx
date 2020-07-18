86407c069ee049759659ea67d48319f1e658770d
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-07-18 23:00:37 +0100

    Credit original logo author

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,6 +5,10 @@ export default () => {
   return (
     <footer className={styles.footer}>
       <p>Reciplease &copy; Rhys Saldanha</p>
+      <p className={styles.smaller}>
+        Using icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
+        href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </p>
     </footer>
   )
 }
