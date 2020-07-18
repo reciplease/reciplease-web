08a62e7e34b41ec4f4cf89409784d0634d7b4b0d
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-07-18 17:12:08 +0100

    Style links in navigation panel

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,7 +4,6 @@ import styles from './navigation.module.css';
 export default () => (
   <nav className={styles.nav}>
     <ul className={styles.list}>
-      <li><a href="/">Ingredients</a></li>
       <li><a href="/">Inventory</a></li>
       <li><a href="/">Recipes</a></li>
       <li><a href="/">Planner</a></li>
