b82d0eefc60b35566a4f42255c5f54cf1b06ae3a
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-07-19 00:14:37 +0100

    Add coffee button

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,17 @@ import styles from './footer.module.css'
 export default () => {
   return (
     <footer className={styles.footer}>
-      <p>Reciplease &copy; Rhys Saldanha</p>
+      <p><span className={styles.title}>Reciplease</span> &copy; Rhys Saldanha</p>
       <p className={styles.smaller}>
         Using icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
         href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
       </p>
+      <a className={styles.bmcButton}
+         target="_blank"
+         rel="noopener noreferrer"
+         href="https://www.buymeacoffee.com/rhyssaldanha"><img
+        src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+        alt="Buy me a coffee"/><span>Buy me a coffee</span></a>
     </footer>
   )
 }
