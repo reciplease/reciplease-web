55ceb993549172dbd64ffa56eeb5130fcc9224a2
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-07-19 15:39:19 +0100

    Resize button

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,9 +12,10 @@ export default () => {
       <a className={styles.bmcButton}
          target="_blank"
          rel="noopener noreferrer"
-         href="https://www.buymeacoffee.com/rhyssaldanha"><img
-        src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
-        alt="Buy me a coffee"/><span>Buy me a coffee</span></a>
+         href="https://www.buymeacoffee.com/rhyssaldanha">
+        <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"/>
+        <span>Buy me a coffee</span>
+      </a>
     </footer>
   )
 }
