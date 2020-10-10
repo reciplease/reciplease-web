b63517d03c5403db53be170a796a7785304c2271
Vikash Kothary <kothary.vikash@gmail.com>
2020-10-10 21:10:42 +0100

    oldclient: Replace components with component library

--- packages/oldclient/src/App.tsx
+++ packages/oldclient/src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/Header';
-import Main from './components/main/Main';
-import Footer from './components/footer/Footer';
-import Layout from './components/layout/Layout';
 import {BrowserRouter} from "react-router-dom";
+
+import { Text, Header, Main, Footer, Layout } from '@reciplease/components';
 
 function App() {
   return (
