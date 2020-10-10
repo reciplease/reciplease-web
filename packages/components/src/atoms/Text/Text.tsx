1e5c60023190dafe59e0e28d1dbb05fb367971c2
Vikash Kothary <kothary.vikash@gmail.com>
2020-10-10 15:13:51 +0100

    components: Add Text component placeholder

---
+++ packages/components/src/atoms/Text/Text.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Text as DefaultText, View as DefaultView } from 'react-native';
+
+const Text : React.FunctionComponent = () => (
+	<h1>Testing12</h1>
+);
+
+export default Text;
---
+++ packages/components/src/index.tsx
@@ -0,0 +1,7 @@
+
+// Atoms
+export { default as Text } from './atoms/Text/Text';
+
+// Molecules
+
+// Organisms
