a14cd833253cf56636da37dd109906bbdc56f589
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-12-30 01:21:27 +0000

    Move nav bar to bottom and stick

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.css';
+import styles from './App.module.css';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
-import Footer from './components/footer/Footer';
 import Layout from './components/layout/Layout';
 import {BrowserRouter} from 'react-router-dom';
+import Navigation from './components/navigation/Navigation';
 
 function App() {
     return (
         <BrowserRouter>
             <Layout>
                 <Header/>
-                <Main/>
-                <Footer/>
+                <Main className={styles.content}/>
+                <Navigation className={styles.stickBottom}/>
             </Layout>
         </BrowserRouter>
     );
