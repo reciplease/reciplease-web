a15218565616e1302e24cbc7c5d0a95f62d03873
Rhys Saldanha <rhys.saldanha@gmail.com>
2021-01-13 22:48:24 +0000

    Fix function name

--- src/api/IngredientService.ts
+++ src/api/IngredientService.ts
@@ -1,12 +1,12 @@
 import {API_ROOT} from './config';
 import {selector} from 'recoil';
 
-const useIngredientList = async (): Promise<Ingredient[]> => {
+const getIngredients = async (): Promise<Ingredient[]> => {
     const response = await fetch(`${API_ROOT}/api/ingredients`, {headers: {Accept: 'application/json'}});
     return await response.json();
 };
 
 export const ingredients = selector<Ingredient[]>({
     key: 'Ingredients',
-    get: useIngredientList
+    get: getIngredients
 });
