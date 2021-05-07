386992b2db2be572a65cd85fcfbca90e71053af8
Rhys Saldanha <rhys.saldanha@gmail.com>
2021-05-08 00:11:16 +0100

    Tidy footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,9 +5,10 @@ const Footer = () => (
     <footer className={styles.footer}>
         <p>Reciplease &copy; Rhys Saldanha</p>
         <p className={styles.smaller}>
-            Using icons made by <a href="https://www.flaticon.com/authors/freepik"
-                                   title="Freepik">Freepik</a> from <a
-            href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+            {'Using icons made by '}
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
+            {' from '}
+            <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
         </p>
     </footer>
 );
